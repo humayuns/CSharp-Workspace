993ebdcdcfd44625abded9c5cb73902740883070
Humayun Shabbir <humayun_shabbir@hotmail.com>
2017-10-03 00:38:24 +0500

    added comments

--- Diary/2017/October/3.cs
+++ Diary/2017/October/3.cs
@@ -1,3 +1,7 @@
+// Auto print without dialog
+// https://stackoverflow.com/questions/17590896/auto-print-without-dialog
+
+
 //Instead of the PrintDialog class, try using the PrintDocument class directly, where you can set the printer by the name:
 
 using System.Drawing.Printing;
